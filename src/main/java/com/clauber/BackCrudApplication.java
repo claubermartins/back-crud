3f7e331d73ebe1b89105bc0d83bd463bc97b4f16
“claubermartins” <clauber789@gmail.com>
2023-01-20 17:42:49 -0300

    15: Listar Cursos (API HTTP GET)

--- src/main/java/com/clauber/BackCrudApplication.java
+++ src/main/java/com/clauber/BackCrudApplication.java
@@ -1,7 +1,12 @@
 package com.clauber;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.clauber.model.Course;
+import com.clauber.repository.CourseRepository;
 
 @SpringBootApplication
 public class BackCrudApplication {
@@ -9,5 +14,17 @@ public class BackCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackCrudApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			
+			courseRepository.save(c);
+		};
+	}
 }
