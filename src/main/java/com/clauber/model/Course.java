64914c45da03aec9de7862a93c072f6db7c77349
“claubermartins” <clauber789@gmail.com>
2023-03-17 15:12:25 -0300

    Remoção Lógica (Soft Delete)

--- src/main/java/com/clauber/model/Course.java
+++ src/main/java/com/clauber/model/Course.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,39 +19,30 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@JsonProperty("_id")
 	private Long id;
-	
+
 	@NotBlank
 	@NotNull
 	@Length(min = 5, max = 100)
 	@Column(length = 200, nullable = false)
 	private String name;
-	
+
 	@NotNull
 	@Length(max = 10)
 	@Pattern(regexp = "Back-end|Front-end")
 	@Column(length = 10, nullable = false)
 	private String category;
+
+	@NotNull
+	@Length(max = 10)
+	@Pattern(regexp = "Ativo|Inativo")
+	@Column(length = 10, nullable = false)
+	private String status = "Ativo";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
