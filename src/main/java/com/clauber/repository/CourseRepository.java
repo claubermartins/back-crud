2d52cf063ff612110cd8a9bec6fcdc6d6496e530
“claubermartins” <clauber789@gmail.com>
2023-03-17 16:21:15 -0300

    37: Spring Boot 3 (Spring 6)

--- src/main/java/com/clauber/repository/CourseRepository.java
+++ src/main/java/com/clauber/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.clauber.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.clauber.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
