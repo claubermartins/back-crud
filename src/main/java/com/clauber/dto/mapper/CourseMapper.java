35a5ce481b95e2b9a08513203743a68608746826
“claubermartins” <clauber789@gmail.com>
2023-04-05 17:12:27 -0300

    42: Padrão Mapper

---
+++ src/main/java/com/clauber/dto/mapper/CourseMapper.java
@@ -0,0 +1,32 @@
+package com.clauber.dto.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.clauber.dto.CourseDTO;
+import com.clauber.model.Course;
+
+@Component
+public class CourseMapper {
+	
+	public CourseDTO toDTO(Course course) {
+		if(course == null) {
+			return null;
+		}
+		return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+	}
+	
+	public Course toEntity(CourseDTO courseDTO) {
+		if (courseDTO == null) {
+			return null;
+		}
+		
+		Course course = new Course();
+		if(courseDTO.id() != null) {
+			course.setId(courseDTO.id());
+		}
+		course.setName(courseDTO.name());
+		course.setCategory(courseDTO.category());
+		course.setStatus("Ativo");
+		return course;
+	}
+}
